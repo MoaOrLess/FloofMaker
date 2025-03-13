9086466cd2cf5e5e3ae03de2fe9b23d026ae341a
MoaOrLess <82501535+MoaOrLess@users.noreply.github.com>
2025-03-13 12:10:24 +0100

    Mouth and Eye buttons control seperate sprites

--- SCENES/learning_1b.gd
+++ SCENES/learning_1b.gd
@@ -13,40 +13,60 @@ extends Node2D
 @onready var BUTTON_OFF = preload("res://ART/Button Off.png")
 @onready var BUTTON_ON = preload("res://ART/Button On.png")
 
-var current_sprite: Sprite2D = null
+var current_sprite_eye: Sprite2D = null
+var current_sprite_mouth: Sprite2D = null
 var texture_item: TextureRect
 signal click
 
 
 func _ready() -> void:
-	spawn_button()
-	spawn_texture(EYE_STARE)
+	spawn_button_eye()
 	spawn_button_mouth()
+	spawn_texture_eye(EYE_STARE)
+	spawn_texture_mouth(MOUTH_KIRBY)
+	
 
 
 func _process(delta: float) -> void:
 	pass
 
-func spawn_texture(text):
+func spawn_texture_eye(text_eye):
 	#Remove sprite if it exists
-	if current_sprite != null:
-		remove_child(current_sprite)
-		current_sprite.queue_free()  
+	if current_sprite_eye != null:
+		remove_child(current_sprite_eye)
+		current_sprite_eye.queue_free()  
 	
 	#Make a new sprite
-	var sprite_inst = Sprite2D.new()
-	add_child(sprite_inst)
+	var sprite_inst_eye = Sprite2D.new()
+	add_child(sprite_inst_eye)
 	
 	#spirte_inst.set_texture(EYE_STARE)
-	sprite_inst.texture = text
-	sprite_inst.scale = Vector2(0.5,0.5)
-	sprite_inst.position = Vector2(100,100)
+	sprite_inst_eye.texture = text_eye
+	sprite_inst_eye.scale = Vector2(0.5,0.5)
+	sprite_inst_eye.position = Vector2(100,100)
 	
 	#Store new sprite
-	current_sprite = sprite_inst
+	current_sprite_eye = sprite_inst_eye
 	
+func spawn_texture_mouth(text_mouth):
+	#Remove sprite if it exists
+	if current_sprite_mouth != null:
+		remove_child(current_sprite_mouth)
+		current_sprite_mouth.queue_free()  
+	
+	#Make a new sprite
+	var sprite_inst_mouth = Sprite2D.new()
+	add_child(sprite_inst_mouth)
+	
+	#spirte_inst.set_texture(EYE_STARE)
+	sprite_inst_mouth.texture = text_mouth
+	sprite_inst_mouth.scale = Vector2(0.5,0.5)
+	sprite_inst_mouth.position = Vector2(100,300)
+	
+	#Store new sprite
+	current_sprite_mouth = sprite_inst_mouth
 
-func spawn_button():
+func spawn_button_eye():
 	#Makes Gridcontainer
 	var grid_container = GridContainer.new()
 	add_child(grid_container)
@@ -55,9 +75,9 @@ func spawn_button():
 	grid_container.position = Vector2(600,300)
 	
 	
-	create_button(grid_container, BUTTON_OFF, EYE_STARE)
-	create_button(grid_container, BUTTON_OFF, EYE_CLASSIC)
-	create_button(grid_container, BUTTON_OFF, EYE_ROUND)
+	create_button_eye(grid_container, BUTTON_OFF, EYE_STARE)
+	create_button_eye(grid_container, BUTTON_OFF, EYE_CLASSIC)
+	create_button_eye(grid_container, BUTTON_OFF, EYE_ROUND)
 	
 func spawn_button_mouth():
 	var grid_container = GridContainer.new()
@@ -65,24 +85,52 @@ func spawn_button_mouth():
 	grid_container.scale = Vector2(0.5,0.5)
 	grid_container.columns = 3
 	grid_container.position = Vector2(600,100)
-	create_button(grid_container, BUTTON_OFF, MOUTH_TREE)
-	create_button(grid_container, BUTTON_OFF, MOUTH_KIRBY)
-	create_button(grid_container, BUTTON_OFF, MOUTH_TOOF)
+	create_button_mouth(grid_container, BUTTON_OFF, MOUTH_TREE)
+	create_button_mouth(grid_container, BUTTON_OFF, MOUTH_KIRBY)
+	create_button_mouth(grid_container, BUTTON_OFF, MOUTH_TOOF)
 
-func create_button(parent, button_texture, eye_texture):
-	var button_inst = Button.new()
-	parent.add_child(button_inst)
-	button_inst.flat = true
-	var text_inst = TextureRect.new()
-	text_inst.position = Vector2(0,20)
+func create_button_eye(parent, button_texture, eye_texture):
+	var button_inst_eye = Button.new()
+	parent.add_child(button_inst_eye)
+	button_inst_eye.flat = true
+	var text_inst_eye = Sprite2D.new()
 	
-	button_inst.icon = button_texture
-	button_inst.add_child(text_inst)
-	text_inst.texture = eye_texture
 	
-	button_inst.pressed.connect(button_is_pressed.bind(text_inst.texture))
+	button_inst_eye.icon = button_texture
+	button_inst_eye.add_child(text_inst_eye)
+	text_inst_eye.texture = eye_texture
 	
+	var button_height = button_inst_eye.icon.get_height()
+	var button_width = button_inst_eye.icon.get_width()
+	text_inst_eye.position = Vector2(button_width/2,button_height/2)
+	text_inst_eye.scale = Vector2(0.80,0.80)
+	
+	button_inst_eye.pressed.connect(button_is_pressed_eye.bind(text_inst_eye.texture))
 
-func button_is_pressed(texture):
-	spawn_texture(texture)
+func create_button_mouth(parent, button_texture, mouth_texture):
+	var button_inst_mouth = Button.new()
+	parent.add_child(button_inst_mouth)
+	button_inst_mouth.flat = true
+	var text_inst_mouth = Sprite2D.new()
+	
+	
+	button_inst_mouth.icon = button_texture
+	button_inst_mouth.add_child(text_inst_mouth)
+	text_inst_mouth.texture = mouth_texture
+	
+	var button_height = button_inst_mouth.icon.get_height()
+	var button_width = button_inst_mouth.icon.get_width()
+	text_inst_mouth.position = Vector2(button_width/2,button_height/2)
+	text_inst_mouth.scale = Vector2(0.80,0.80)
+	
+	button_inst_mouth.pressed.connect(button_is_pressed_mouth.bind(text_inst_mouth.texture))
+
+func button_is_pressed_eye(texture):
+	spawn_texture_eye(texture)
+	print(str(texture))
+	print('Button pressed, sprite updated')
+	
+func button_is_pressed_mouth(texture):
+	spawn_texture_mouth(texture)
+	print(str(texture))
 	print('Button pressed, sprite updated')
