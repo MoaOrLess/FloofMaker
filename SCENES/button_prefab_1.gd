751d9088698c625c9ce80d3f0c86af8a70a9a2b1
MoaOrLess <82501535+MoaOrLess@users.noreply.github.com>
2025-03-26 21:11:30 +0100

    Button Prefab made in Godot UI

---
+++ SCENES/button_prefab_1.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var option_sprite: Sprite2D = $OptionButton/OptionSprite
+var pos_offset = Vector2(10,10)
+var base_pos = Vector2(143,143)
+
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print(option_sprite.position)
+
+
+func _on_option_button_pressed() -> void:
+	pass
+
+func _on_option_button_button_up() -> void:
+	option_sprite.position = base_pos
+
+
+func _on_option_button_button_down() -> void:
+	option_sprite.position = base_pos + pos_offset
