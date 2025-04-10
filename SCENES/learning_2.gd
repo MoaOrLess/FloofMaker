41b24a94dade5dfbcac292ea5069b97b06566618
MoaOrLess <82501535+MoaOrLess@users.noreply.github.com>
2025-04-10 19:00:06 +0200

    New art and Pages kinda work

--- SCENES/learning_2.gd
+++ SCENES/learning_2.gd
@@ -18,11 +18,29 @@ var EYE_BIG = preload("res://ART/Eye Big.png")
 var EYE_DONE = preload("res://ART/Eye Done.png")
 var EYE_LINE_ = preload("res://ART/Eye Line .png")
 var EYE_WOBLE = preload("res://ART/Eye Woble.png")
+var EYE_ANIME = preload("res://ART/Eye Anime.png")
+var EYE_BLINK = preload("res://ART/Eye Blink.png")
+var EYE_CROSSED = preload("res://ART/Eye Crossed.png")
+var EYE_GURL = preload("res://ART/Eye Gurl.png")
+var EYE_INTENSE = preload("res://ART/Eye Intense.png")
+var EYE_KIRBY = preload("res://ART/Eye Kirby.png")
+var EYE_SHARP = preload("res://ART/Eye Sharp.png")
+var EYE_TIC_TAC = preload("res://ART/Eye TicTac.png")
+var EYE_TRAUMA = preload("res://ART/Eye Trauma.png")
+var EYE_TWINKLE = preload("res://ART/Eye Twinkle.png")
+var EYE_U = preload("res://ART/Eye U.png")
 
 
 var MOUTH_KIRBY = preload("res://ART/Mouth Kirby.png")
 var MOUTH_TOOF = preload("res://ART/Mouth Toof.png")
 var MOUTH_TREE = preload("res://ART/Mouth Tree.png")
+var MOUTH_LIP = preload("res://ART/Mouth Lip.png")
+var MOUTH_POUT = preload("res://ART/Mouth Pout.png")
+var MOUTH_SMOLLSMILE = preload("res://ART/Mouth Smollsmile.png")
+var MOUTH_V = preload("res://ART/Mouth V.png")
+var MOUTH_W = preload("res://ART/Mouth W.png")
+var MOUTH_BEAN = preload("res://ART/Mouth Bean.png")
+
 
 const BODY_ICON = preload("res://ART/Body Icon.png")
 const BROW_ICON = preload("res://ART/Brow Icon.png")
@@ -33,9 +51,15 @@ const MOUTH_ICON = preload("res://ART/Mouth Icon.png")
 const NOSE_ICON = preload("res://ART/Nose Icon.png")
 
 
-var EyeOption = [EYE_STARE, EYE_ROUND, EYE_CLASSIC, EYE_BIG, EYE_DONE,EYE_LINE_,EYE_WOBLE]
-var MouthOption = [MOUTH_KIRBY, MOUTH_TOOF, MOUTH_TREE]
+var EyeOption = [EYE_STARE, EYE_ROUND, EYE_CLASSIC, EYE_BIG, EYE_DONE, EYE_LINE_,EYE_WOBLE, EYE_ANIME,
+EYE_BLINK, EYE_CROSSED, EYE_GURL, EYE_INTENSE, EYE_KIRBY, EYE_SHARP, EYE_TIC_TAC, EYE_TRAUMA, EYE_TWINKLE, EYE_U]
+
+var MouthOption = [MOUTH_KIRBY, MOUTH_TOOF, MOUTH_TREE, MOUTH_LIP, MOUTH_POUT, MOUTH_SMOLLSMILE, MOUTH_V,
+MOUTH_W, MOUTH_BEAN]
+
 var CategoryOption = [BODY_ICON, EYE_ICON, MOUTH_ICON, BROW_ICON, NOSE_ICON, HAIR_ICON, EXTRA_ICON]
+
+var count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -45,15 +69,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(count)
 
 func create_eye_option_buttons():
-	for eye_sprite in EyeOption:
-		var button = BUTTON_PREFAB.instantiate()
-		var sprite = button.get_node("OptionButton/OptionSprite")
-		sprite.texture = eye_sprite
-		option_container.add_child(button)
+	for page_index in 9:
+			var button = BUTTON_PREFAB.instantiate()
+			var sprite = button.get_node("OptionButton/OptionSprite")
+			sprite.texture = EyeOption[count]
+			option_container.add_child(button)
+			count += 1
+			if count > EyeOption.size(): 
+				break
 
+
+func clear_eye_option_buttons():
+	for child in option_container.get_children():
+		option_container.remove_child(child)
+	
 
 func create_mouth_option_buttons():
 	for mouth_sprite in MouthOption:
@@ -74,3 +106,17 @@ func create_Icon_option_buttons():
 
 func page_mouth():
 	create_mouth_option_buttons()
+
+
+func _on_texture_button_2_pressed() -> void:
+	print("pressed")
+	count = 9
+	clear_eye_option_buttons()
+	create_eye_option_buttons()
+
+
+func _on_texture_button_pressed() -> void:
+	print("pressed")
+	count = 0
+	clear_eye_option_buttons()
+	create_eye_option_buttons()
